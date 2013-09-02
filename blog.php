3fcc8dcc31de1b9d34fde6ded40cab30fe58419c
Chris Opperwall <copperwall@calpoly.edu>
2013-09-02 03:14:32 +0000

    New Blog Feature
    Added a new php page that is going to be the beginning of a new blog
    project. In it's current state, it reads from a mysql database and
    posts all "posts" to the page.
    
    The backend is in a semi-functional state, so new developments will
    mostly be on the HTML/CSS side.

---
+++ blog.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+   <?php
+      // Create Connection
+      $con=mysqli_connect("localhost", "me",  "", "Test");
+
+      // Check connection
+      if (mysqli_connect_errno($con))
+      { 
+         echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+      }
+
+      $result = mysqli_query($con,"SELECT * FROM blog");
+      
+      while($row = mysqli_fetch_array($result))
+      {
+         echo $row['PostTitle'];
+         echo "<br>";
+         echo "By: " . $row['Author'];
+         echo "<br/><br/>";
+         echo $row['PostBody'];
+         echo "<br/><br/>";
+      }
+
+      mysqli_close($con);
+   ?>
+</body>
+</html>
