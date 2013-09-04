209d983d8a76846b4e0ed4ccc0b86035bd4a840f
Chris Opperwall <copperwall@calpoly.edu>
2013-09-04 01:46:00 +0000

    Added notes for a header and added a container div to contain the posts in

--- blog.php
+++ blog.php
@@ -1,31 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <title>The Blog</title>
+   <link rel='stylesheet' type='text/css' href='bootstrap/css/bootstrap.css'/>
+   <link rel='stylesheet' type='text/css' href='blog_style.css'/>
 </head>
 <body>
-   <?php
-      // Create Connection
-      $con=mysqli_connect("localhost", "me",  "", "Test");
+   <!-- The Header should go hear, code background, "The Blog" title -->
+   <!-- NavBar is on the side? Either that or list of blog titles -->
+   <header>
+      <h1>The Blog</h1>
+   </header>
+   <div class='container'>
+      <?php
+         // Create Connection
+         $con=mysqli_connect("localhost", "me",  "", "Test");
 
-      // Check connection
-      if (mysqli_connect_errno($con))
-      { 
-         echo "Failed to connect to MYSQL: " . mysqli_connect_error();
-      }
+         // Check connection
+         if (mysqli_connect_errno($con))
+         { 
+            echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+         }
 
-      $result = mysqli_query($con,"SELECT * FROM blog");
-      
-      while($row = mysqli_fetch_array($result))
-      {
-         echo "<h3>" . $row['PostTitle'] . "</h3>";
-         echo "<br>";
-         echo "By: " . $row['Author'];
-         echo "<br/><br/>";
-         echo $row['PostBody'];
-         echo "<br/><br/>";
-      }
+         $result = mysqli_query($con,"SELECT * FROM blog");
+         
+         while($row = mysqli_fetch_array($result))
+         {
+            echo "<h3>" . $row['PostTitle'] . "</h3>";
+            echo "<br>";
+            echo "By: " . $row['Author'];
+            echo "<br/><br/>";
+            echo $row['PostBody'];
+            echo "<br/><br/>";
+         }
 
-      mysqli_close($con);
-   ?>
+         mysqli_close($con);
+      ?>
+   </div>
 </body>
 </html>
