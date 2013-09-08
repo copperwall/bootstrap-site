23765ba07a93e25e409f5de25b707827be3155be
Chris Opperwall <copperwall@calpoly.edu>
2013-09-08 03:40:59 +0000

    Beginning to build space for a side/navbar
    The main container div for the blog was replaced by a container-fluid
    div. It now has two columns, with the one of the left beign for a
    nav/info bar and the right one being for posted content.

--- blog.php
+++ blog.php
@@ -12,32 +12,39 @@
       <h1>The Blog</h1>
    </header>
    <br/><br/>
-   <div class='container'>
-      <?php
-         // Create Connection
-         $con=mysqli_connect("localhost", "me",  "", "Test");
+   <div class='container-fluid'>
+      <div class='row-fluid'>
+         <div class='span2'>
+            <!-- Nav or description goes here -->
+         </div>
+         <div class='span 10'>
+         <?php
+            // Create Connection
+            $con=mysqli_connect("localhost", "me",  "", "Test");
 
-         // Check connection
-         if (mysqli_connect_errno($con))
-         { 
-            echo "Failed to connect to MYSQL: " . mysqli_connect_error();
-         }
+            // Check connection
+            if (mysqli_connect_errno($con))
+            { 
+               echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+            }
 
-         $result = mysqli_query($con,"SELECT * FROM blog");
-         
-         while($row = mysqli_fetch_array($result))
-         {
-            echo "<div class='container post'>";
-            echo "<h3>" . $row['PostTitle'] . "</h3>";
-            echo $row['PostDate'];
-            echo "<br/><br/>";
-            echo $row['PostBody'];
-            echo "</div>";
-            echo "<br/><br/>";
-         }
+            $result = mysqli_query($con,"SELECT * FROM blog");
+            
+            while($row = mysqli_fetch_array($result))
+            {
+               echo "<div class='row'>";
+               echo "<div class='span6 offset2 post'>";
+               echo "<h3>" . $row['PostTitle'] . "</h3>";
+               echo $row['PostDate'];
+               echo "<br/><br/>";
+               echo $row['PostBody'];
+               echo "</div>";
+               echo "</div>";
+               echo "<br/><br/>";
+            }
 
-         mysqli_close($con);
-      ?>
+            mysqli_close($con);
+         ?>
    </div>
 </body>
 </html>
